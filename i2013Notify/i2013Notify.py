a6b963f9b3ef36d460648f6f66d2167ccb20c948
Шумихин Иван <shumidze@gmail.com>
2015-01-28 00:29:56 +0400

    исправления для django

--- i2013Notify/i2013Notify.py
+++ i2013Notify/i2013Notify.py
@@ -2,19 +2,48 @@
 
 import requests
 
+from .exception import i2013NotifyError
+
+
+try:
+    from django.conf import settings
+
+    if not settings.configured:
+        settings.configure()
+    dj = True
+except:
+    dj = False
+
 
 class i2013Notify(object):
     """Sending message to hubs of the company by xmpp protocol"""
 
-    def __init__(self, url='', login='', passwd='', referer='python notyfy', debug=False):
-        self.url = url
-        self.login = login
-        self.passwd = passwd
-        self.referer = referer
-        self.debug = debug
+    def __init__(self, url=None, login=None, passwd=None, referer='python notyfy', debug=False):
+        if dj == False:
+            self.url = url
+            self.login = login
+            self.passwd = passwd
+            self.referer = referer
+            self.debug = debug
+        else:
+            self.url = getattr(settings, 'I_2013_NOTIFY_URL', url)
+            self.login = getattr(settings, 'I_2013_NOTIFY_LOGIN', login)
+            self.passwd = getattr(settings, 'I_2013_NOTIFY_PASSWD', passwd)
+            self.referer = getattr(settings, 'I_2013_NOTIFY_REFERER', referer)
+            self.debug = getattr(settings, 'I_2013_NOTIFY_DEBUG', debug)
+        if self.url is None or len(self.url) == 0:
+            raise i2013NotifyError("url must be a string greater than zero, {0} given".format(str(self.url)))
+        if self.login is None or len(self.login) == 0:
+            raise i2013NotifyError("login must be a string greater than zero, {0} given".format(str(self.login)))
+        if self.passwd is None or len(self.passwd) == 0:
+            raise i2013NotifyError("passwd must be a string greater than zero, {0} given".format(str(self.passwd)))
 
 
-    def sendMessage(self, message='error message', hub=''):
+    def send_message(self, message='error message', hub=None):
+        if message is None or len(message) < 6:
+            raise i2013NotifyError("message must be a string greater than 6, {0} given".format(str(self.passwd)))
+        if dj == True:
+            hub = getattr(settings, 'I_2013_NOTIFY_HUB', hub)
         hubUri = "http://{0}/hubs/{1}/message".format(self.url, hub)
         fields = {
             "message[text]": message
